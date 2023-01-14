a0ad5e22c34c146f88c53b562fd738646e73f59c
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2023-01-13 23:45:21 -0300

    Update aula4-pandaEseaborn.py

--- aula4-pandaEseaborn.py
+++ aula4-pandaEseaborn.py
@@ -51,7 +51,30 @@ modelo_arvoredecisao.fit(x_treino, y_treino)
 
 RandomForestRegressor()
 
-# Continuar do tempo 1h15
+previsao_regressaolinear = modelo_regressaolinear.predict(x_teste)
+previsao_arvoredecisao = modelo_arvoredecisao.predict(x_teste)
+
+from sklearn.metrics import r2_score
+print(r2_score(y_teste, previsao_regressaolinear))
+print(r2_score(y_teste, previsao_arvoredecisao))
+
+
+tabela_auxiliar = pd.DataFrame()
+tabela_auxiliar["y_teste"] = y_teste
+tabela_auxiliar["Previsao Arvore Decisao"] = previsao_arvoredecisao
+tabela_auxiliar["Previsao Regressao Linear"] = previsao_regressaolinear
+
+print(tabela_auxiliar)
+
+sns.lineplot(data=tabela_auxiliar)
+plt.show()
+
+nova_tabela = pd.read_csv("Aula4/novos.csv")
+print(nova_tabela)
+
+
+previsao = modelo_arvoredecisao.predict(nova_tabela)
+print(previsao)
 
 
 # Passo2: Entendimento da Área/Empresa
